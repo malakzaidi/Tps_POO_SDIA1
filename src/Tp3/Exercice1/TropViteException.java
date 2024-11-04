9dff879646c80e261cef51debae2482b03bc8483
malakzaidi <malakzaidi815@gmail.com>
2024-11-04 10:35:17 +0100

    Adding comments

--- src/Tp3/Exercice1/TropViteException.java
+++ src/Tp3/Exercice1/TropViteException.java
@@ -1,7 +1,10 @@
 package Tp3.Exercice1;
 
+// Classe TropViteException héritant de la classe Exception
 public class TropViteException  extends Exception {
+    // Constructeur prenant en paramètre un entier vitesse
     public TropViteException(int vitesse ) {
+        // Appel du super constructeur 
         super ("C'est une exception de type TropViteException TropViteException. Vitesse en cause : " + vitesse );
     }
 
